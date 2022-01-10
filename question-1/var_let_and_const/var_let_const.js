5d89d0ac855320978285e8cb38115f8df6d3a07d
byeileen <55578454+byeileen@users.noreply.github.com>
2022-01-10 12:49:46 +0100

    Add files via upload

---
+++ question-1/var_let_and_const/var_let_const.js
@@ -0,0 +1,17 @@
+// Difference between var, let and const
+
+// var
+var greet = "hello with var";
+var greet ="changed greet";
+console.log(greet)
+
+//let
+let greetL = "hello with let";
+//greetL = "hello change with let";
+//let greetL = "new let";
+console.log(greetL)
+
+//const
+const greetC = "hello with const"
+greetC = "changed greet";
+console.log(greetC);
