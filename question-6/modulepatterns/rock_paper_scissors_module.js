e32e0e8ded0eb29147453a80742213b4cc596315
Josefine-Schougaard <55578452+Josefine-Schougaard@users.noreply.github.com>
2022-01-10 16:22:08 +0100

    Add files via upload

---
+++ question-6/modulepatterns/rock_paper_scissors_module.js
@@ -0,0 +1,69 @@
+var myGame = (function () {
+
+	// "private" variables (only visible in module scope)
+	var computerChoice, userChoice, showUserChoice, showComputerChoice, showResult;
+
+	showResult = document.getElementById('result');
+	showUserChoice = document.getElementById('player_choice');
+	showComputerChoice = document.getElementById('computer_choice');
+
+	// functions
+	var computerInput, compare, display;
+
+	// private functions  (only visible in module scope, cannot be called from outside the module)
+	computerInput = function () {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				computerChoice = 'Rock';
+				break;
+			case 1:
+				computerChoice = 'Paper';
+				break;
+			case 2:
+				computerChoice = 'Scissors';
+				break;
+		}
+		// console.log('Computer: ' + computerChoice);
+	};
+
+	compare = function () {
+		if (userChoice === computerChoice) {
+			showResult.innerHTML = 'Game is a draw!';
+		} else if (userChoice === 'Paper' && computerChoice === 'Rock') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Paper' && computerChoice === 'Scissors') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (userChoice === 'Scissors' && computerChoice === 'Rock') {
+			showResult.innerHTML = 'Computer wins!';
+		} else if (userChoice === 'Scissors' && computerChoice === 'Paper') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Rock' && computerChoice === 'Scissors') {
+			showResult.innerHTML = 'You win!';
+		} else if (userChoice === 'Rock' && computerChoice === 'Paper') {
+			showResult.innerHTML = 'Computer wins!';
+		}
+	};
+
+	display = function () {
+		showUserChoice.innerHTML = userChoice;
+		showComputerChoice.innerHTML = computerChoice;
+	}
+
+	return {
+		// public functions
+		play: function () {
+			userChoice = this.id;
+			console.log('User: ' + userChoice);
+			computerInput();
+			display();
+			compare();
+		}
+	};
+
+	// module end //
+})();
+
+var buttons = document.getElementsByClassName('button');
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', myGame.play);
+}
