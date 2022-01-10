6a8b510c7aa71d8c35506ea206f473025f240111
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2022-01-10 14:48:54 +0100

    select multiple elements from the DOM + different ways to select

---
+++ question-5/selecting-multiple-and-elements-from-dom/main.js
@@ -0,0 +1,18 @@
+//get ID's
+const id = document.getElementById("demo");
+console.log(id);
+
+//Using Class
+const intro = document.getElementsByClassName("intro");
+console.log(intro);
+
+//Get TAG's
+const usingTags = document.getElementsByTagName("body");
+console.log(usingTags);
+
+//Multiple DOM elements selector
+const multipleClasses = document.querySelectorAll(".child");
+console.log(multipleClasses);
+
+
+
